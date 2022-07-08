8ea74c6b10edad4f23c7520298895d30a439cc04
msheldon32 <mattsheldon914@gmail.com>
2022-07-09 00:59:00 +0100

    scraper update

--- scraper.py
+++ scraper.py
@@ -1,1 +1,37 @@
 # scraper
+
+import threading
+import csv
+import os
+import json
+import requests
+import time
+import datetime
+
+#parameters
+frequency = 15
+
+def pull_gbfs(name, url):
+    print(f"name: {name}, url: {url}")
+    schema = requests.get(url).json()["data"]["en"]["feeds"]
+    for page in schema:
+        with open("{}/{}".format(name,page["name"]), "a") as file:
+            data = requests.get(page["url"]).json()
+            data["date"] = str(datetime.datetime.now())
+            file.write(json.dumps(data))
+            file.write("\n")
+
+def pull_data():
+    all_folders = os.listdir()
+    with open("api_sources") as file:
+        api_reader = csv.reader(file)
+        for row in api_reader:
+            if row[0] not in all_folders:
+                os.mkdir(row[0])
+            row_thread = threading.Thread(target=pull_gbfs, args=row, daemon=True)
+            row_thread.start()
+
+if __name__=="__main__":
+    while True:
+        pull_data()
+        time.sleep(frequency*60)
