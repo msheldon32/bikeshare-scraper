3e44d929728046aaf715c49ac5791301822916f7
msheldon32 <mattsheldon914@gmail.com>
2022-07-09 01:56:35 +0100

    ls

--- scraper.py
+++ scraper.py
@@ -13,6 +13,7 @@ frequency = 15
 
 def pull_gbfs(name, url):
     print(f"name: {name}, url: {url}")
+    print("ls")
     
     schema = requests.get(url).json()["data"]
     schema = list(schema.values())[0]["feeds"]
